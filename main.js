960f528413cae47e2a2d994f48ac88f78dd320c9
Luan Vieira <luanvwittchind@gmail.com>
2024-02-23 20:45:06 -0300

    Enviando arquivos do projeto 2

---
+++ main.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('form-atividade');
+const imgAprovado = `<img src="C:/git/Projeto2/Front-End M8 - Imagens/images/aprovado.png" alt="Emoji celebrando" />`;
+const imgReprovado = `<img src="C:/git/Projeto2/Front-End M8 - Imagens/images//reprovado.png" alt="Emoji decepcionado" />`;
+
+let linhas = '';
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const inputNomeAtividade = document.getElementById('nome-atividade');
+    const inputNotaAtividade = document.getElementById('nota-atividade');
+
+    let linha = '<tr>';
+    linha += `<td>${inputNomeAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value}</td>`;
+    linha += `<td>${inputNotaAtividade.value >= 7 ? imgAprovado : imgReprovado}</td>`;
+    linha += '</tr>';
+
+    linhas += linha;
+
+    const corpoTabela = document.querySelector('tbody');
+    corpoTabela.innerHTML = linhas;
+
+    inputNomeAtividade.value = '';
+    inputNotaAtividade.value = '';
+
+
+    
+});
